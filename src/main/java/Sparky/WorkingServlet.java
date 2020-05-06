837779a7b109b4e1aa33823f4be164d5ed9c7063
Luke Major <luke.major@my.wheaton.edu>
2020-05-06 15:49:17 -0500

    "second push"

--- src/main/java/Sparky/WorkingServlet.java
+++ src/main/java/Sparky/WorkingServlet.java
@@ -8,15 +8,36 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.SparkConf;
+
 @WebServlet(value = "/working")
 public class WorkingServlet extends HttpServlet {
+	//JavaSparkContext cont;
+	//JavaRDD<String> dataSetRDD;
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException  {
 		try {
 			res.getWriter().println("I am working.");
+			
+		//	JavaRDD<String> dataSetRDD = cont.textFile("ASheet.csv");
+		//	JavaRDD<Integer> dataSetLines = dataSetRDD.map( f -> f.length() );
+		//	res.getWriter().println(dataSetRDD);
 		} finally {
 			
 		}
+	}
+	
+	@Override
+	public void init() throws ServletException {
+		//JavaSparkContext cont;
+		JavaRDD<String> dataSetRDD;
+		SparkConf conf = new SparkConf().setAppName("CsvTest").setMaster("local");
+		//cont = new JavaSparkContext(conf);
+		//dataSetRDD = cont.textFile("ASheet.csv");
+		
 	}
 
 }
